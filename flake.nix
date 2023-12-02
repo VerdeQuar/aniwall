ef44b4921c3b7ec3c881f850575aaa5f244d333b
VerdeQuar <verdequar@gmail.com>
2023-12-02 22:28:58 +0100

    Add flake support

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          # rustVersion = "1.70.0";
+          rustChannel = "nightly";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          aniwall = (rustPkgs.workspace.aniwall {});
+          default = packages.aniwall;
+        };
+      }
+    );
+}
